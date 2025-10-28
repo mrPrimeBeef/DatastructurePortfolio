451a86bdcdc548d7606ab263b9add134e25e40c4
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-10-28 16:51:42 +0100

    start dynamic

--- DynamicArray/DynamicArray.js
+++ DynamicArray/DynamicArray.js
@@ -1,3 +1,19 @@
+import { StaticArray } from "../StaticArray/staticarray.js";
+
 export default class StaticArray {
-    
+  #array; // Det "skjulte" statiske array
+  #size; // Antal faktiske elementer (brugte pladser)
+  #capacity; // Aktuel kapacitet
+
+  // capacity = 4 default if none given
+  constructor(capacity = 4) {
+    this.#capacity = capacity;
+    this.#array = new StaticArray(this.#capacity);
+    this.#size = 0;
+  }
+
+  get length(){
+    return this.#size;
+  }
+
 }
