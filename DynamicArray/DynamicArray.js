89facfd522966b0ee49e3c354fef59937bd4350b
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-10-29 13:48:15 +0100

    test passed

--- DynamicArray/DynamicArray.js
+++ DynamicArray/DynamicArray.js
@@ -1,6 +1,6 @@
-import { StaticArray } from "../StaticArray/staticarray.js";
+import StaticArray from "../StaticArray/staticarray.js";
 
-export default class StaticArray {
+export default class DynamicArray {
   #array; // Det "skjulte" statiske array
   #size; // Antal faktiske elementer (brugte pladser)
   #capacity; // Aktuel kapacitet
@@ -12,8 +12,84 @@ export default class StaticArray {
     this.#size = 0;
   }
 
-  get length(){
+  size() {
     return this.#size;
   }
 
+  capacity() {
+    return this.#capacity;
+  }
+
+  get(index) {
+    this.#_checkIndex(index);
+    return this.#array.get(index);
+  }
+
+  set(index, item) {
+    this.#_checkIndex(index);
+    return this.#array.set(index, item);
+  }
+
+  add(item) {
+    if (this.#size >= this.#capacity) {
+      this.grow();
+    }
+    this.#array[this.#size] = item;
+    this.#size++;
+  }
+
+  grow() {
+    const newCapacity = this.#capacity * 2;
+    const newArray = new StaticArray(newCapacity);
+
+    for (let i = 0; i < this.#size; i++) {
+      newArray[i] = this.#array[i];
+    }
+    this.#array = newArray;
+    this.#capacity = newCapacity;
+  }
+
+  insert(index, item) {
+    if (index < 0 || index > this.#size) {
+      throw new RangeError("Index out of bounds");
+    }
+
+    if (this.#size >= this.#capacity) {
+      this.grow();
+    }
+
+    for (let i = this.#size; i > index; i--) {
+      this.#array[i] = this.#array[i - 1];
+    }
+
+    this.#array[index] = item;
+    this.#size++;
+  }
+
+  remove(index) {
+     this.#_checkIndex(index);
+
+    for (let i = index; i < this.#size - 1; i++) {
+        this.#array[i] = this.#array[i + 1];
+    }
+
+
+    this.#array[this.#size - 1] = undefined;
+
+    this.#size--;
+  }
+
+  clear(){
+    const newArray = new StaticArray();
+    this.#size = 0;
+    this.#array = newArray;
+  }
+
+  shrink() {}
+
+  #_checkIndex(index) {
+    if (index < 0 || index >= this.#size) {
+      throw new RangeError("Index out of bounds");
+    }
+  }
 }
