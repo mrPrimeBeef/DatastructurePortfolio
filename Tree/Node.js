102f856dc8620d0c0551172fe61d86535f3c97f7
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-11-23 15:33:05 +0100

    start tree

---
+++ Tree/Node.js
@@ -0,0 +1,24 @@
+export class Tree {
+  #_parent;
+  #_childNodes;
+  #_value;
+
+  // - `parent` - som henviser til denne node’s eneste parent
+  // - `childNodes` - en liste over alle children denne node har
+  // - `value` - som indeholder denne node’s *data*
+
+  constructor() {}
+
+//   - `firstChild()` - der returnerer den første i listen af childNodes
+// - `lastChild()` - der returnerer den sidste i listen af childNodes
+// - `hasChildNodes()` - der fortæller hvorvidt den har childNodes eller ej
+// - `appendChild( child )` - der tilføjer child til listen af children
+// - `removeChild( child )` - der fjerner child fra listen af children
+// - `replaceChild( newChild, oldChild )` - der fjerner oldChild fra listen, og tilføjer newChild på dens plads
+
+// For alle metoder gælder at de både skal opdatere parent og childNodes i alle berørte nodes.
+
+// Du bestemmer selv om du vil lave metoder uden parametre som metoder eller getter-properties.
+}
+
+
---
+++ Tree/Tree.js
@@ -0,0 +1,13 @@
+export class Tree{
+#_root;
+
+constructor(){
+
+}
+
+
+// - `dump()` - der udskriver hele træet i consollen.
+// - `addValue( value )` - der opretter en ny node med den givne value, og tilføjer den et sted i træet - du bestemmer selv hvor!
+// - `findValue( value )` - der leder efter den givne value i træet, og returnerer den (første) Node der har den
+// - `removeValue( value )` - der finder og fjerner Noden med den givne value fra træet
+}
